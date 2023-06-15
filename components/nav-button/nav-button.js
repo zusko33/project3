958ee09f6694444dbda740b0a2073d980dd99e65
Zuzanna Skokowska <zuzanna.skokowska@icloud.com>
2023-06-15 17:18:25 +0200

    adding pagination

---
+++ components/nav-button/nav-button.js
@@ -0,0 +1,10 @@
+import { navigation } from "./index.js";
+import { prevButton } from "./index.js";
+import { nextButton } from "./index.js";
+
+prevButton.addEventListener("click", (e) => {
+  console.log("click");
+});
+nextButton.addEventListener("click", () => {
+  console.log("click");
+});
